e8d4e9f8a48543b6ff5da7ab1b153f3105f14a1a
Sam Hokin <shokin@ncgr.org>
2020-03-27 09:16:09 -0600

    Not done but adding for posterity.

---
+++ org/ncgr/intermine/bio/web/displayer/GWASCircle.java
@@ -0,0 +1,114 @@
+package org.ncgr.intermine.bio.web.displayer;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.intermine.api.InterMineAPI;
+import org.intermine.api.query.PathQueryExecutor;
+import org.intermine.api.results.ExportResultsIterator;
+import org.intermine.api.results.ResultElement;
+
+import org.intermine.model.InterMineObject;
+
+import org.intermine.objectstore.ObjectStoreException;
+
+import org.intermine.pathquery.Constraints;
+import org.intermine.pathquery.OrderDirection;
+import org.intermine.pathquery.PathQuery;
+
+import org.intermine.web.displayer.ReportDisplayer;
+import org.intermine.web.logic.config.ReportDisplayerConfig;
+import org.intermine.web.logic.results.ReportObject;
+
+/**
+ * Generates data for a CanvasXpress circle plot of p-values versus marker position.
+ *
+ * @author Sam Hokin
+ */
+public class GWASCircle extends ReportDisplayer {
+
+    /**
+     * Construct with config and the InterMineAPI.
+     *
+     * @param config to describe the report displayer
+     * @param im the InterMine API
+     */
+    public GWASCircle(ReportDisplayerConfig config, InterMineAPI im) {
+        super(config, im);
+    }
+
+    @Override
+    public void display(HttpServletRequest request, ReportObject reportObject) {
+
+        InterMineObject gwas = reportObject.getObject();
+        String gwasIdentifier;
+        try {
+            gwasIdentifier = (String) gwas.getFieldValue("primaryIdentifier");
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException("Error getting primaryIdentifier.", ex);
+        }
+
+        PathQueryExecutor executor = im.getPathQueryExecutor();
+
+        // get the markers and phenotypes from the GWASResults
+        List<String> markerList = new LinkedList<>();
+        List<String> chromosomeList = new LinkedList<>();
+        List<String> phenotypeList = new LinkedList<>();
+        List<Integer> positionList = new LinkedList<>();
+        List<Double> log10pList = new LinkedList<>();
+        // GWASResult.marker.primaryIdentifier,            // 0
+        // GWASResult.marker.chromosome.primaryIdentifier, // 1
+        // GWASResult.marker.chromosome.length,            // 2
+        // GWASResult.marker.chromosomeLocation.start,     // 3
+        // GWASResult.marker.chromosomeLocation.end,       // 4
+        // GWASResult.phenotype.primaryIdentifier,         // 5
+        // GWASResult.pValue                               // 6
+        PathQuery query = new PathQuery(im.getModel());
+        query.addViews(
+                       "GWASResult.marker.primaryIdentifier",            // 0
+                       "GWASResult.marker.chromosome.primaryIdentifier", // 1
+                       "GWASResult.marker.chromosome.length",            // 2
+                       "GWASResult.marker.chromosomeLocation.start",     // 3
+                       "GWASResult.marker.chromosomeLocation.end",       // 4
+                       "GWASResult.phenotype.primaryIdentifier",         // 5
+                       "GWASResult.pValue"                               // 6
+                       );
+        query.addConstraint(Constraints.eq("GWASResult.study.primaryIdentifier", gwasIdentifier));
+        query.addOrderBy("GWASResult.marker.chromosome.primaryIdentifier", OrderDirection.ASC);
+        query.addOrderBy("GWASResult.marker.primaryIdentifier", OrderDirection.ASC);
+        ExportResultsIterator results;
+        try {
+            results = executor.execute(query);
+        } catch (ObjectStoreException ex) {
+            throw new RuntimeException("Error retrieving data.", ex);
+        }
+        while (results.hasNext()) {
+            List<ResultElement> row = results.next();
+            String markerIdentifier = (String) row.get(0).getField();
+            String chromosomeIdentifier = (String) row.get(1).getField();
+            Integer chromosomeLength = (Integer) row.get(2).getField();
+            Integer markerStart = (Integer) row.get(3).getField();
+            Integer markerEnd = (Integer) row.get(4).getField();
+            String phenotypeIdentifier = (String) row.get(5).getField();
+            Double pValue = (Double) row.get(6).getField();
+            // derived quantities
+            int position = (markerStart+markerEnd)/2;
+            Double log10p = -Math.log10(pValue);
+            // add to lists
+            markerList.add(markerIdentifier);
+            chromosomeList.add(chromosomeIdentifier);
+            phenotypeList.add(phenotypeIdentifier);
+            positionList.add(position);
+            log10pList.add(log10p);
+        }
+
+        // send the data on its way
+        request.setAttribute("markerList", markerList);
+        request.setAttribute("chromosomeList", chromosomeList);
+        request.setAttribute("phenotypeList", phenotypeList);
+        request.setAttribute("positionList", positionList);
+        request.setAttribute("log10pList", log10pList);
+    }
+}
